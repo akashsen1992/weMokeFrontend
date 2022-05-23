0f23e7c66fb37beda866e2967c637af4cd58e479
akku19 <akashsen127@gmail.com>
2022-05-23 13:06:27 +0530

    yes

--- src/App.js
+++ src/App.js
@@ -6,17 +6,20 @@ import {
 } from "react-router-dom";
 import Home from './Home';
 import Contact from './Contact';
+import Header from './component/Header';
+import Footer from './component/Footer';
 
 
 function App() {
   return (
     <BrowserRouter>
+    <Header />
     <Routes>
-    <Route exact path="/" element={<Home />}>
-        <Route exact path='/contact' element={<Contact/>} /> 
-
+        <Route exact path="/" element={<Home />}>
+        <Route exact path='/contact' element={<Contact/>} />
       </Route>
     </Routes>
+    <Footer />
   </BrowserRouter>
   );
 }
